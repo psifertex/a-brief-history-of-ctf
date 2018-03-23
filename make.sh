4246e3a0ac35ffeb3820cd542d8474653fe5f5ff
Jordan Wiens <jordan@psifertex.com>
2018-03-23 01:56:26 +0100

    make now pushes gh-pages

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf docs/*
 reveal-md briefhistory.md --scripts js/reveal.js,js/Chart.bundle.min.js,js/chart.js --css css/v35.css --static docs --static-dirs images
+git subtree push --prefix docs origin gh-pages
